f07f7d012b854eaff5cd2bc28228ed0467cc0b4a
ange.picard <ange.picard@davikingcode.com>
2021-03-31 09:46:16 +0200

    feat: add selected icon

--- src/mdi-icon-picker.ts
+++ src/mdi-icon-picker.ts
@@ -21,6 +21,9 @@ export class MdiIconPicker extends LitElement {
     @internalProperty()
     searchResults: Icon[] = [];
 
+    @internalProperty()
+    selectedIcon?: Icon;
+
     searchIndex: lunr.Index = lunr(() => { });
     iconMap: Map<string, Icon> = new Map();
 
@@ -54,12 +57,39 @@ export class MdiIconPicker extends LitElement {
         console.log(this.searchResults)
     }
 
+    _handleClick(icon: Icon) {
+        this.selectedIcon = icon;
+        console.log(this.selectedIcon);
+    }
+
     // Define the element's template
     render() {
         return html`
             <link rel=stylesheet href="${this.fontUrl}">
+            <style>
+                .icon-btn {
+                    display: inline-flex;
+                    height: 32px;
+                    width: 32px;
+                    border-radius: 4px;
+                    align-items: center;
+                    justify-content: center;
+                    cursor: pointer;
+                }
+                .icon-btn:hover {
+                    background-color: #e4e4ea;
+                }
+            </style>
+            ${this.selectedIcon !== undefined
+                ? html`<span class="mdi mdi-${this.selectedIcon.name}"></span>`
+                : ''
+            }
             <input @keyup="${this._launchSearch}" />
-            ${this.searchResults.slice(0, 100).map(i => html`<span class="mdi mdi-${i.name}"></span>`)}
+            ${this.searchResults.slice(0, 100).map(i => html`
+                <div class="icon-btn" @click="${() => this._handleClick(i)}">
+                    <span class="mdi mdi-${i.name}"></span>
+                </div>
+            `)}
         `;
     }
 }
